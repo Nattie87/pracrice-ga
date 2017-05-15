9e84b98075a4da0d8f7e23f0a485ea186285c0b9
Nattie87 <natalie.huitson@gmail.com>
2017-05-15 11:58:03 +0100

    gulp file set up

--- reactTutorial/my-react-project/gulpfile.js
+++ reactTutorial/my-react-project/gulpfile.js
@@ -8,3 +8,23 @@ var minifyCSS    = require('gulp-clean-css');
 var uglify       = require('gulp-uglify');
 var rename       = require('gulp-rename');
 var changed      = require('gulp-change');
+
+
+var SCSS_SRC     = './src/Assets/scss/**/*.scss';
+var SCSS_DEST    = './src/Assets/css';
+
+gulp.task('compile_scss', function(){
+  gulp.src(SCSS_SRC)
+  .pipe(sass().on('error', sass.logError))
+  .pipe(minifyCSS())
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(changed(SCSS_DEST))
+  .pipe(gulp.dest(SCSS_DEST));
+
+});
+
+gulp.task('watch_scss', function() {
+  gulp.watch(SCSS_SRC, ['compile_scss']);
+});
+
+gulp.task('default', ['watch_scss']);
