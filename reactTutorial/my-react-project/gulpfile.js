8a5cacef0e014ae0be7f594805ffd8f1759c305e
Nattie87 <natalie.huitson@gmail.com>
2017-05-15 12:50:40 +0100

    sorted out bastard of a gulp problem

--- reactTutorial/my-react-project/gulpfile.js
+++ reactTutorial/my-react-project/gulpfile.js
@@ -7,13 +7,14 @@ var sass         = require('gulp-sass');
 var minifyCSS    = require('gulp-clean-css');
 var uglify       = require('gulp-uglify');
 var rename       = require('gulp-rename');
-var changed      = require('gulp-change');
+var changed      = require('gulp-changed');
 
 
 var SCSS_SRC     = './src/Assets/scss/**/*.scss';
 var SCSS_DEST    = './src/Assets/css';
 
 gulp.task('compile_scss', function(){
+
   gulp.src(SCSS_SRC)
   .pipe(sass().on('error', sass.logError))
   .pipe(minifyCSS())
