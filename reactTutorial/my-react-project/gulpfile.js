872a5532f4e91d6c0477622fa876cfea07da4fb2
Nattie87 <natalie.huitson@gmail.com>
2017-05-15 11:52:24 +0100

    dependencies sorted, now requiring them

---
+++ reactTutorial/my-react-project/gulpfile.js
@@ -0,0 +1,10 @@
+'use strict';
+
+//dependencies i have installed
+
+var gulp         = require('gulp');
+var sass         = require('gulp-sass');
+var minifyCSS    = require('gulp-clean-css');
+var uglify       = require('gulp-uglify');
+var rename       = require('gulp-rename');
+var changed      = require('gulp-change');
