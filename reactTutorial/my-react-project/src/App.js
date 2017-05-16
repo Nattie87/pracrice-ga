89bfc74157dfe41616cc7af80bd85419adfee628
Nattie87 <natalie.huitson@gmail.com>
2017-05-16 16:22:45 +0100

    setting up my components

--- reactTutorial/my-react-project/src/App.js
+++ reactTutorial/my-react-project/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Header from './components/headerComponent/header';
+import './Assets/css/default.min.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Test
+        <Header />
       </div>
     );
   }
