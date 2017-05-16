a3339f08bfee26a710bb639800d627243b524101
Nattie87 <natalie.huitson@gmail.com>
2017-05-16 17:40:19 +0100

    styling

--- reactTutorial/my-react-project/src/App.js
+++ reactTutorial/my-react-project/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import Header from './components/headerComponent/header';
 import './Assets/css/default.min.css';
+import Footer from './components/footerComponent/footer';
+import Homepage from './components/pages/homePage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Homepage />
+        <Footer />
       </div>
     );
   }
---
+++ reactTutorial/my-react-project/src/components/footerComponent/footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        Nattie
+      </footer>
+
+    );
+  }
+}
+
+export default Footer;
