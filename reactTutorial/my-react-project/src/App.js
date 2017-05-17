521dcfc521544b48223555c5a9e83c93aaabc4ab
Nattie87 <natalie.huitson@gmail.com>
2017-05-17 14:46:17 +0100

    sorting out the routing

--- reactTutorial/my-react-project/src/App.js
+++ reactTutorial/my-react-project/src/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 import './Assets/css/default.min.css';
 import Header     from './components/headerComponent/header';
 import Footer     from './components/footerComponent/footer';
 import Homepage   from './components/pages/homePage';
+import Products   from './components/pages/products';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
         <Header />
-        <Homepage />
+        <Route exact path='/' component={Homepage} />
+        <Route exact path='/Products' component={Products} />
+
         <Footer />
       </div>
+      </Router>
     );
   }
 }
